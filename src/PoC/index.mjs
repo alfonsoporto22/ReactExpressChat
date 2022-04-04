aa32e62a87ccb5426726128c1bbc3103aeea67ad
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-04 12:14:07 +0200

    Show all messages

--- src/PoC/index.mjs
+++ src/PoC/index.mjs
@@ -41,8 +41,8 @@ function newUserHandler(ev) {
     ev.preventDefault();
     let user = document.querySelector("#usuario");
     let pass = document.querySelector("#password");
-    let datafail = { userName: user.value, password: pass.value };
-    const data = JSON.stringify(datafail);
+    let dataTaken = { userName: user.value, password: pass.value };
+    const data = JSON.stringify(dataTaken);
     newUser(host, data);
 
 }
@@ -55,7 +55,7 @@ loginButton.addEventListener("click", newUserHandler);
 
 const crearToken = document.querySelector("#crearToken");
 let id = document.querySelector("#id");
-let secret = document.querySelector("#password2")
+let secret = document.querySelector("#secret")
 const htmlGetTokens = document.querySelector("#getTokens");
 
 function authToken(id, secret) {
@@ -68,25 +68,26 @@ function authToken(id, secret) {
     return `Basic ${base64token}`;
 }
 
-function newAuthoHandler(ev){
+function newAuthoHandler(ev) {
     ev.preventDefault();
-    htmlGetTokens.innerText= authToken(id, secret);
+    htmlGetTokens.innerText = authToken(id, secret);
 
 }
 
-crearToken.addEventListener("click",newAuthoHandler);
+crearToken.addEventListener("click", newAuthoHandler);
 
 /**
  * GET con autenticación
  */
 
- const token = authToken(id,secret);
- //authGet(url+"/messages/", token);
+const htmlGetTokensMessages = document.querySelector("#htmlGetTokensMessages");
+const botonGetMensajes = document.querySelector("#getMensajes");
 
- async function authGet(url, token) {
+
+async function authGet(url, token) {
     const response = await fetch(
         url,
-        { 
+        {
             headers: {
                 Authorization: token
             }
@@ -95,3 +96,12 @@ crearToken.addEventListener("click",newAuthoHandler);
     const data = await response.json();
     return data;
 }
+function newAuthoGetHandler(ev) {
+    const token = authToken(id.value, secret.value);
+    authGet(host + "/messages/", token).then(
+        data => htmlGetTokensMessages.innerHTML = JSON.stringify(data)
+    )
+    
+}
+
+botonGetMensajes.addEventListener("click", newAuthoGetHandler);
